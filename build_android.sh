84bbab6b0b07b2026d861be7ef0a07875ecc92c4
Jomy10 <jonas.vbs4@gmail.com>
2021-11-15 20:44:25 +0100

    Nieuwe build framework voor Android

--- build_android.sh
+++ build_android.sh
@@ -2,12 +2,17 @@
 
 # Build file for building the Android library
 
+# Build script
+cd /Users/jonaseveraert/AndroidStudioProjects/tVoetje
+./gradlew cargoBuild
+
+## Deprecated ##
 # Remove recursively
-rm -r target/aarch64-linux-android
-rm -r target/armv7-linux-androideabi
-rm -r target/i686-linux-android
+#rm -r target/aarch64-linux-android
+#rm -r target/armv7-linux-androideabi
+#rm -r target/i686-linux-android
 
 # Build commands
-cargo build --target aarch64-linux-android --release 
-cargo build --target armv7-linux-androideabi --release
-cargo build --target i686-linux-android --release
+#cargo build --target aarch64-linux-android --release 
+#cargo build --target armv7-linux-androideabi --release
+#cargo build --target i686-linux-android --release
